5af11e07f4c0d2ba90323c15ac2e1a927e309d3b
杨国涣 <46448013+yangguohuan@users.noreply.github.com>
2025-02-20 23:14:57 +0800

    Add files via upload

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Users(models.Model):
+
+    username = models.CharField('用户名', max_length=30, unique=True)
+
+    password = models.CharField('密码', max_length=32)
+
+    date_added = models.DateTimeField('创建时间', auto_now_add=True)
+
+    update_date = models.DateTimeField('更新时间', auto_now=True)
+
+    def __str__(self):
+        return f"用户：{self.username} + {self.password}"
